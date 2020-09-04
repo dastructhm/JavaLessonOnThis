952d69fe92941253efa9c9b30820cec7890b55d3
bjdhjy888 <bjdhjy678@gmail.com>
2020-09-04 15:02:23 +0800

    Initial commit

---
+++ src/LessonOnThis.java
@@ -0,0 +1,11 @@
+class Age {
+    int dateOfBirth = 1982;
+
+    public int CalculateAge(int dateOfBirth){
+        return dateOfBirth;
+    }
+
+    public int CalculateAge2(int dateOfBirth){
+        return this.dateOfBirth;
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        Age myAge = new Age();
+        System.out.println(myAge.CalculateAge(2020));
+        System.out.println(myAge.CalculateAge2(2020));
+    }
+}
+
