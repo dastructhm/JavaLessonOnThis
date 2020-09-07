a774023654550168f8789e92325b0e5fe690627f
bjdhjy888 <bjdhjy678@gmail.com>
2020-09-07 15:15:17 +0800

    calling member variable

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ public class main {
     public static void main(String[] args) {
         Age myAge = new Age();
         System.out.println(myAge.CalculateAge(2020));
-        System.out.println("Local variable: " + myAge.CalculateAge2(2020));
+        System.out.println("Calling member variable: " + myAge.CalculateAge2(2020));
     }
 
 
