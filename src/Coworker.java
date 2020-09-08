a51140e7152100ab3a9f72baca5cd98db01786a8
bjdhjy888 <bjdhjy678@gmail.com>
2020-09-08 15:40:10 +0800

    111111

--- src/Coworker.java
+++ src/Coworker.java
@@ -10,6 +10,7 @@ class Coworker {
         System.out.println(" But using This keyword, you are " + (2020 - this.YearOfBirth) + " years old.");
     }
 
+
     public void CalculateNominalAge(int YearOfBirth){
         System.out.print("your nominal age is " + (2020 - YearOfBirth + 1) + " years old.");
         System.out.println(" But using This keyword, you are " + (2020 - this.YearOfBirth) + " years old.");
