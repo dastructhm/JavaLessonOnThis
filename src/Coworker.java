f684412093a920255052162849b3fc3df90d8b69
bjdhjy888 <bjdhjy678@gmail.com>
2020-09-08 15:06:06 +0800

    new22

--- src/Coworker.java
+++ src/Coworker.java
@@ -1,5 +1,5 @@
 class Coworker {
-    int YearOfBirth = 2008;
+    int YearOfBirth = 2010;
 
     public Coworker(){
         this(2000);
@@ -7,14 +7,12 @@ class Coworker {
 
     public Coworker(int YearOfBirth){
         System.out.print("the constructor says you are " + (2020 - YearOfBirth) + " years old.");
-
-        System.out.println(" But using this keyword, you are " + (2020 - this.YearOfBirth) + " years old.");
+        System.out.println(" But using This keyword, you are " + (2020 - this.YearOfBirth) + " years old.");
     }
 
     public void CalculateNominalAge(int YearOfBirth){
         System.out.print("your nominal age is " + (2020 - YearOfBirth + 1) + " years old.");
-
-        System.out.println(" But using this keyword, you are " + (2020 - this.YearOfBirth) + " years old.");
+        System.out.println(" But using This keyword, you are " + (2020 - this.YearOfBirth) + " years old.");
     }
 }
 
