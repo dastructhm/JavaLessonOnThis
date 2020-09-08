5ab97f1921014fc9240dfeb56e1f82084c442582
bjdhjy888 <bjdhjy678@gmail.com>
2020-09-08 13:17:20 +0800

    instance

--- src/Age.java
+++ src/Age.java
@@ -1,13 +1,15 @@
 class Age {
-    int dateOfBirth = 1982;
+    int YearOfBirth = 1990;
 
-    public int CalculateAge(int dateOfBirth){
-        return dateOfBirth;
+    public int CalculateAge(int YearOfBirth){
+        return (2020 - YearOfBirth);
     }
 
-    // myAge.dateOfBirth
-    public int CalculateAge2(int dateOfBirth){
-        return this.dateOfBirth;
+    public int CalculateAgeUsingThis(int YearOfBirth){
+        // this就是myAge
+        // myAge对象是Age类的实例
+        // myAge.dateOfBirth
+        return (2020 - this.YearOfBirth);
     }
 }
 
