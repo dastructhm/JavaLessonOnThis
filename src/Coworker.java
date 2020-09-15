23ef008c3523d18d2e7bb1a09c4a86f327ad930a
bjdhjy888 <bjdhjy678@gmail.com>
2020-09-15 08:43:03 +0800

    comments added

--- src/Coworker.java
+++ src/Coworker.java
@@ -1,22 +1,29 @@
 class Coworker {
+    // Field YearOfBirth
     int YearOfBirth = 2010;
 
     public Coworker(){
         this(2000);
     }
 
-    // Constructor
+    // This is a constructor
+    // Field YearOfBirth is shadowed by a constructor parameter
     public Coworker(int YearOfBirth){
         System.out.print("the constructor says you are " + (2020 - YearOfBirth) + " years old.");
 
         // 2010 is used
+        // To represent the current instance of the class in which it appears
+        // To access class variables and methods
         System.out.println(" But using This keyword, you are " + (2020 - this.YearOfBirth) + " years old.");
     }
 
+    // Field YearOfBirth is shadowed by a method parameter
     public void CalculateNominalAge(int YearOfBirth){
         System.out.print("your nominal age is " + (2020 - YearOfBirth + 1) + " years old.");
 
         // 2010 is used again
+        // To represent the current instance of the class in which it appears
+        // To access class variables and methods
         System.out.println(" But using This keyword, you are " + (2020 - this.YearOfBirth) + " years old.");
     }
 }
